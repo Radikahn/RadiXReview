784f5efd6ea9b70218c8ce1942acc4fba2af850f
Radman <radikahn@gmail.com>
2025-01-31 19:52:21 -0800

    Alpha 1.0.3

--- link_builder.py
+++ link_builder.py
@@ -1,29 +1,40 @@
 from serpapi import GoogleSearch
 from dotenv import load_dotenv
 import os
+import json
 
 class LinkBuilder:
 
-    search_term: str
+    def pull_links() -> dict:
 
-    load_dotenv()
+        load_dotenv()
 
-    api_key = os.getenv('API')
-   
+        api_key = os.getenv('API')
+
+
+        params = GoogleSearch({
+            "q": "coffee", 
+            "location": "San Jose,California",
+            "device": "desktop",
+            "api_key": api_key
+        })
+
+        results = params.get_dict()
+
+        return results["organic_results"]
     
 
-    params = GoogleSearch({
-        "q": "coffee", 
-        "location": "San Jose,California",
-        "tbm": "xxx",
-        "output": "json",
-        "device": "desktop",
-        "api_key": api_key
-    })
 
-    search = GoogleSearch(params)
-    results = search.get_dict()
-    organic_results = results["organic_results"]
+    def write_to_json(links: dict):
+        file = open("Web Files/link_results.json", 'w+')
+        
+        file.write(json.dumps(links, indent = 4))
+        
+        file.close()
+
+        
+
+LinkBuilder.write_to_json(LinkBuilder.pull_links())
 
 
 
